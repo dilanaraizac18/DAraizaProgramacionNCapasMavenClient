ef9bc73c6eaa558937610a0123ed035866feb10a
Dilan <Dilan@DESKTOP-12CB95S>
2026-03-27 13:35:43 -0600

    Security and authentication 2.7

--- src/main/java/com/digis01/DAraizaProgramacionNCapasMaven/LoginController.java
+++ src/main/java/com/digis01/DAraizaProgramacionNCapasMaven/LoginController.java
@@ -4,10 +4,22 @@
  */
 package com.digis01.DAraizaProgramacionNCapasMaven;
 
+import com.digis01.DAraizaProgramacionNCapasMaven.ML.Result;
+import com.digis01.DAraizaProgramacionNCapasMaven.ML.Usuario;
+import jakarta.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+import static org.slf4j.helpers.Reporter.error;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
@@ -21,20 +33,64 @@ import org.springframework.web.client.RestTemplate;
 public class LoginController {
     
     
-    private static String RutaBase = "http://localhost:8080";
+    private static String RutaBase = "http://localhost:8080/auth";
+    
     @GetMapping
-    public String Login(@RequestParam(value = "error", required = false) String error,  Model model){
-        RestTemplate restTemplate = new RestTemplate();
-        
-        ResponseEntity<>
-        
-        
-        
+    public String Login (@RequestParam(value = "error", required = false) String error, Model model) {
+         model.addAttribute("usuario", new Usuario());
         if (error != null) {
-            model.addAttribute("loginError", true);
+            model.addAttribute("errorlogin", error);
         }
-        
-        
+
         return "Login";
+
+    }
+    
+    
+    
+    @PostMapping
+    public String PostLogin(@ModelAttribute("username")  String username, @ModelAttribute("password") String password, HttpSession httpSession){
+        Result result = new Result();
+                
+        try{
+                    RestTemplate restTemplate = new RestTemplate();
+                    HttpHeaders httpHeader = new HttpHeaders();
+                    httpHeader.setContentType(MediaType.APPLICATION_JSON);
+                    
+                    Map<String, Object> userbody =  new HashMap<>();
+                    userbody.put("username", username);
+                    userbody.put("password", password);
+                    
+                    HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(userbody, httpHeader);
+
+            ResponseEntity<Result> responseLogin = restTemplate.exchange(RutaBase,
+                    HttpMethod.POST,
+                    requestEntity,
+                    Result.class);
+
+            if (responseLogin.getBody() != null) {
+                Result resultR = responseLogin.getBody();
+                
+                String token = (String) resultR.object;
+                httpSession.setAttribute("token", token);
+
+                if (resultR.correct) {
+                    return "redirect:/usuario";
+                }
+                return "/Login";
+            }
+                    
+                    
+                    
+                    
+            
+            
+        }catch(Exception ex){
+            result.correct = false;
+            result.errorMessage = ex.getLocalizedMessage();
+            result.ex = ex;
+        }
+         
+        return "GetAll";
     }
 }
